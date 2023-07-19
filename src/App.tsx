3a142e5ffc697c822a63d201eaac7652c2ef3498
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-07-19 12:14:17 +0800

    install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
+import "./App.css";
+import img from "./Graphic_Side.jpg";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex w-full">
+      <div className="flex justify-center items-center w-1/2">
+        <div className="border border-gray-200 rounded w-80 h-96"></div>
+      </div>
+      <img src={img} className="w-1/2" />
+    </div>
+  );
 }
 
 export default App;
